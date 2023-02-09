39187044646d7e2437f53ae16c3d4629499bb902
Renan Marques <nan.dev01@gmail.com>
2023-02-09 19:13:58 -0300

    Requisito 5 resolvido

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,70 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
 
 class Home extends React.Component {
+  state = {
+    search: '',
+    productsList: [],
+    emptyArray: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleClick = async () => {
+    const { search } = this.state;
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
+    const response = await fetch(endPoint);
+    const data = await response.json();
+    if (data.results.length === 0) {
+      this.setState({
+        emptyArray: true,
+      });
+    }
+
+    this.setState({
+      search: '',
+      productsList: data.results,
+    });
+  };
+
   render() {
+    const { search, productsList, emptyArray } = this.state;
     return (
       <>
-        <input type="text" />
+        <input
+          data-testid="query-input"
+          type="text"
+          name="search"
+          value={ search }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          teste
+        </button>
         <p
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <div>
+          { productsList.length > 0
+            ? productsList.map((product) => (
+              <ProductCard
+                key={ product.id }
+                product={ product }
+              />
+            )) : emptyArray && <h3>Nenhum produto foi encontrado</h3>}
+        </div>
       </>
     );
   }
