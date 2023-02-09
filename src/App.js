5d6ca412685def228ee96b8406388ee688a73955
fabiotorres95 <engfabiotorres@outlook.com>
2023-02-09 19:33:47 -0300

    criado ShoppingCart, botao redireciona pra ele

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
-      <Route path="/" component={ Home } />
+      <Switch>
+        <Route path="/cart" component={ ShoppingCart } />
+        <Route path="/" component={ Home } />
+      </Switch>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Home extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <>
         <input type="text" />
@@ -10,9 +12,21 @@ class Home extends React.Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <button
+          data-testid="shopping-cart-button"
+          onClick={ () => history.push('/cart') }
+        >
+          Carrinho
+        </button>
       </>
     );
   }
 }
+
+Home.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Home;
