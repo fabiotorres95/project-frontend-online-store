c55a2a8945b236e0d1b5ff035880fd448fc89120
Lílian Vieira <lilianv528@gmail.com>
2023-02-09 19:17:05 -0300

    requisito 4 ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.requirementCategories();
+  }
+
+  requirementCategories = async () => {
+    const data = await getCategories();
+    this.setState({
+      categories: data,
+    });
+  };
+
   render() {
+    const { categories } = this.state;
     return (
-      <>
+      <div>
         <input type="text" />
         <p
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-      </>
+        <p>
+          {categories.map((categorie) => (
+            <label
+              key={ categorie.id }
+              htmlFor={ categorie.id }
+              data-testid="category"
+            >
+              <input
+                type="radio"
+                name="categoria"
+                id={ categorie.id }
+              />
+            </label>
+          ))}
+        </p>
+      </div>
     );
   }
 }
