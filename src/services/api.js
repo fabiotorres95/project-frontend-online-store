0e08383be575eadcf29454b2dd604c3c3a7f17d3
Renan Marques <nan.dev01@gmail.com>
2023-02-10 18:24:38 -0300

    Requisito 6 resolvido

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,11 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return data;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
+export async function getProductById(categoryId) {
+  const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const response = await fetch(endPoint);
+  const data = await response.json();
+
+  return data.results;
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
